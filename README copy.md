0c1858923f74e891b6efcc444bf780d289f3be2c
Nadia Hashemi <nadiahashemi@Nadias-MacBook-Pro-2.local>
2024-12-07 21:44:42 -0500

    added files and yml file

---
+++ README copy.md
@@ -0,0 +1,2 @@
+# Github-Actions-C20
+University of Richmond Bootcamp - Code Challenge 20 - GitHub Actions CI/CD Setup
---
+++ server/src/seeds/cleanDb.ts
@@ -0,0 +1,19 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+
+export default async (modelName: "Question", collectionName: string) => {
+  try {
+    if (!models[modelName].db || !models[modelName].db.db) { // error fix
+      throw new Error(`Model or database not found for modelName: ${modelName}`);
+    }
+    let modelExists = await models[modelName].db.db.listCollections({
+      name: collectionName
+    }).toArray();
+
+    if (modelExists.length) {
+      await db.dropCollection(collectionName);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
